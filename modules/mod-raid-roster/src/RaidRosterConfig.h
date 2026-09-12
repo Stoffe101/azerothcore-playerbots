875cd8107aa49ba9cb05aa5618af5c1168f9f2b7
Skrra <94893570+Stoffe101@users.noreply.github.com>
2026-09-12 09:35:45 +0200

    guild director: add configuration state

--- modules/mod-raid-roster/src/RaidRosterConfig.h
+++ modules/mod-raid-roster/src/RaidRosterConfig.h
@@ -12,5 +12,11 @@ extern uint32 g_AdventureStartLevel;
 extern uint8 g_AdventureStartProgression;
 extern bool g_AdventureStartRevealMap;
 
+// Natural guild-chat group director. Uses the persistent roster rather than creating a second
+// population of disposable dungeon bots.
+extern bool g_GuildDirectorEnable;
+extern bool g_GuildDirectorAutoTravel;
+extern uint32 g_GuildDirectorReadyTimeoutMs;
+
 void RaidRosterLoadConfig();
 #endif
