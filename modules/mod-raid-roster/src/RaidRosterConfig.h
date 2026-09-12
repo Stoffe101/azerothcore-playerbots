a983f8fe65bb25b68f3dd091d3c42616c31d3b01
Skrra <94893570+Stoffe101@users.noreply.github.com>
2026-09-12 08:25:41 +0200

    feat: add adventure start configuration

--- modules/mod-raid-roster/src/RaidRosterConfig.h
+++ modules/mod-raid-roster/src/RaidRosterConfig.h
@@ -1,7 +1,16 @@
 #ifndef MOD_RAID_ROSTER_CONFIG_H
 #define MOD_RAID_ROSTER_CONFIG_H
 
+#include "Define.h"
+
 extern bool g_RaidRosterEnable;
+
+// New-player bootstrap for this fork. Kept separate from raid-roster behaviour so it can be
+// disabled independently without affecting persistent raid bots.
+extern bool g_AdventureStartEnable;
+extern uint32 g_AdventureStartLevel;
+extern uint8 g_AdventureStartProgression;
+extern bool g_AdventureStartRevealMap;
 
 void RaidRosterLoadConfig();
 #endif
