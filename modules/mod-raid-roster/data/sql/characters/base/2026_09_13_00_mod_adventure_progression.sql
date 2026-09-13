964f99a56dc7d6c5134fe4e6f757d9f8c19dcfad
Skrra <94893570+Stoffe101@users.noreply.github.com>
2026-09-13 16:20:17 +0200

    Add starter profile to adventure progression schema

--- modules/mod-raid-roster/data/sql/characters/base/2026_09_13_00_mod_adventure_progression.sql
+++ modules/mod-raid-roster/data/sql/characters/base/2026_09_13_00_mod_adventure_progression.sql
@@ -3,6 +3,7 @@ CREATE TABLE IF NOT EXISTS `mod_adventure_progression` (
   `starter_initialized` TINYINT UNSIGNED NOT NULL DEFAULT 0,
   `starter_gear_granted` TINYINT UNSIGNED NOT NULL DEFAULT 0,
   `starter_spec_tab` TINYINT UNSIGNED NOT NULL DEFAULT 255,
+  `starter_profile` TINYINT UNSIGNED NOT NULL DEFAULT 0,
   `pending_caches` SMALLINT UNSIGNED NOT NULL DEFAULT 0,
   `last_level_cache` TINYINT UNSIGNED NOT NULL DEFAULT 0,
   PRIMARY KEY (`player_guid`)
