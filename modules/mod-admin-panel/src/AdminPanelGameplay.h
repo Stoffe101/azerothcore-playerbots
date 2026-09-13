4242ef5ca076626d5cf38fd96d843c93c551d0fa
Skrra <94893570+Stoffe101@users.noreply.github.com>
2026-09-13 17:00:35 +0200

    Add admin gameplay utility interface

---
+++ modules/mod-admin-panel/src/AdminPanelGameplay.h
@@ -0,0 +1,37 @@
+#ifndef MOD_ADMIN_PANEL_GAMEPLAY_H
+#define MOD_ADMIN_PANEL_GAMEPLAY_H
+
+#include "Define.h"
+
+class Player;
+
+namespace AdminPanelGameplay
+{
+struct PopulationStats
+{
+    uint32 sessions = 0;
+    uint32 realPlayers = 0;
+    uint32 bots = 0;
+    uint32 botTarget = 0;
+    uint32 botBatch = 0;
+    float botActivity = 0.0f;
+};
+
+PopulationStats GetPopulationStats();
+void SetBotTarget(uint32 target, uint32 batch = 10);
+void SetBotActivity(float percent);
+
+bool GiveGold(Player* player, uint32 gold);
+void Repair(Player* player);
+void Restore(Player* player);
+void MaxSkills(Player* player);
+void RefreshConsumables(Player* player);
+void ResetEraTalents(Player* player);
+void RegearTbcPreRaid(Player* player);
+
+uint32 PrepareGroup(Player* leader);
+uint32 SummonGroup(Player* leader);
+uint32 RaidNight(Player* leader);
+}
+
+#endif
