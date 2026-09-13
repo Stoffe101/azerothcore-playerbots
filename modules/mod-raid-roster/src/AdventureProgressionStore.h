cdd2aa1b206c841cc45cdb0f831816833edfb74f
Skrra <94893570+Stoffe101@users.noreply.github.com>
2026-09-13 16:19:52 +0200

    Track adventure starter profile per character

--- modules/mod-raid-roster/src/AdventureProgressionStore.h
+++ modules/mod-raid-roster/src/AdventureProgressionStore.h
@@ -10,13 +10,15 @@ struct State
     bool starterInitialized = false;
     bool starterGearGranted = false;
     uint8 starterSpecTab = 255;
+    uint8 starterProfile = 0;
     uint16 pendingCaches = 0;
     uint8 lastLevelCache = 0;
 };
 
 State LoadOrCreate(uint32 playerGuid);
-void MarkStarterInitialized(uint32 playerGuid);
+void MarkStarterInitialized(uint32 playerGuid, uint8 profile = 0);
 void MarkStarterGearGranted(uint32 playerGuid, uint8 specTab);
+void PrepareStarterProfile(uint32 playerGuid, uint8 profile);
 void AddPendingCache(uint32 playerGuid, uint16 count = 1);
 bool ConsumePendingCache(uint32 playerGuid);
 void SetLastLevelCache(uint32 playerGuid, uint8 level);
