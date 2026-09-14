37d375b5f52b05d39934debfd712dfe0ba9bae4c
Skrra <94893570+Stoffe101@users.noreply.github.com>
2026-09-14 02:55:32 +0200

    titan: register deterministic affix runtime

--- modules/mod-titan-rune/src/TitanRuneLoader.cpp
+++ modules/mod-titan-rune/src/TitanRuneLoader.cpp
@@ -1,8 +1,11 @@
 #include "TitanRuneSystem.h"
 #include "Log.h"
 
+void AddTitanRuneAffixScripts();
+
 void Addmod_titan_runeScripts()
 {
     LOG_INFO("server.loading", "[TitanRune] Registering Titan Rune dungeon scripts.");
     AddTitanRuneScripts();
+    AddTitanRuneAffixScripts();
 }
