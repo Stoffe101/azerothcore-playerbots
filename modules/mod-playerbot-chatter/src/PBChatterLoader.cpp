beaafa6a6e599bb8df48fc4ddf4bb038fdb37c1b
Skrra <94893570+Stoffe101@users.noreply.github.com>
2026-09-14 23:36:09 +0200

    Remove duplicate chatter raid leader

--- modules/mod-playerbot-chatter/src/PBChatterLoader.cpp
+++ modules/mod-playerbot-chatter/src/PBChatterLoader.cpp
@@ -6,7 +6,6 @@
 #include "PBChatterWorld.h"
 #include "PBChatterEvents.h"
 #include "PBChatterCommand.h"
-#include "PBChatterRaidLeader.h"
 #include "PBAIGuildLife.h"
 #include "PBAIGuildAutonomy.h"
 
@@ -19,5 +18,4 @@ void Addmod_playerbot_chatterScripts()
     PBChatterMakeEventScript();
     AddPBAIGuildLifeScripts();
     AddPBAIGuildAutonomyScripts();
-    AddPBChatterRaidLeaderScripts();
 }
