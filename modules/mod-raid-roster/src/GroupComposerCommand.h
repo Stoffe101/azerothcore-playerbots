00ca9b209f8a2d2ade3e3fcb8c2a5ad7624c709c
Skrra <94893570+Stoffe101@users.noreply.github.com>
2026-09-16 00:09:41 +0200

    feat(group-composer): extend command surface for complete composer flow

--- modules/mod-raid-roster/src/GroupComposerCommand.h
+++ modules/mod-raid-roster/src/GroupComposerCommand.h
@@ -12,12 +12,21 @@ public:
 
     static bool HandleBegin(ChatHandler* handler, std::string mode, std::string activity, std::string difficulty,
         uint32 size, uint32 tanks, uint32 healers, uint32 dps, uint32 preferGuild, uint32 fillWorld,
-        uint32 keepMe, uint32 balanceClasses, uint32 avoidDuplicates, uint32 minimumItemLevel);
+        uint32 keepMe, uint32 balanceClasses, uint32 balanceUtility, uint32 balanceRange,
+        uint32 avoidDuplicates, uint32 minimumItemLevel);
     static bool HandlePreference(ChatHandler* handler, std::string role, std::string classToken,
         std::string spec, std::string strength);
+    static bool HandleHumanRole(ChatHandler* handler, std::string name, std::string role);
+    static bool HandleHuman(ChatHandler* handler, std::string name, std::string role);
+    static bool HandlePin(ChatHandler* handler, std::string name, std::string role, std::string strength);
+    static bool HandleArrangePreference(ChatHandler* handler, std::string name, uint32 subgroup);
+
     static bool HandleFind(ChatHandler* handler);
     static bool HandleArrange(ChatHandler* handler);
+    static bool HandleMove(ChatHandler* handler, std::string name, uint32 subgroup);
     static bool HandleAssemble(ChatHandler* handler);
+    static bool HandleQueue(ChatHandler* handler);
+    static bool HandleDiagnostics(ChatHandler* handler);
     static bool HandleClear(ChatHandler* handler);
     static bool HandleStatus(ChatHandler* handler);
 };
