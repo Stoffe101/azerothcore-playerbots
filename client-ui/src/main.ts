d57107a4638610afe641d65f6de15aeac2afbd72
Skrra <94893570+Stoffe101@users.noreply.github.com>
2026-09-18 08:24:28 +0200

    chore(group-composer): bump modern UI API to 0.3.0

--- client-ui/src/main.ts
+++ client-ui/src/main.ts
@@ -17,7 +17,7 @@ function ensureDashboard(): any {
 }
 
 _G.GroupComposerModernUI = {
-    version: "0.2.1",
+    version: "0.3.0",
     dashboard: undefined,
     ensureDashboard: () => ensureDashboard(),
     createBuildSelector: (...args: Parameters<typeof createBuildSelector>) => createBuildSelector(...args),
